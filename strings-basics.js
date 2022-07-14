4c861f204326872685adee20c022b49417021db4
Work <Work@Jessicas-MacBook-Air-2.local>
2022-07-14 11:34:59 -0400

    strings

---
+++ strings-basics.js
@@ -0,0 +1,6 @@
+const message = 'Hello Javascript Students'
+const msgToLowerCase = message.toLowerCase()
+const msgToUpperCase= message.toUpperCase()
+
+console.log(message)
+const mySchool = 'Boca Code'
